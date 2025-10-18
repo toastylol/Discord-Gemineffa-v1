11eff3ff40791827997b04b53d98eacb6c48f481
toastylol <leonardjohn974@gmail.com>
2025-10-18 11:59:45 +0530

    create uptime command

---
+++ commands/uptime.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
+const { formatDuration } = require('../utils.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('uptime')
+        .setDescription('Displays how long Ineffa has been online.'),
+
+        async execute(interaction) {
+            const uptime = formatDuration(interaction.client.uptime);
+
+            const embed = new EmbedBuilder()
+                .setColor('Random')
+                .setTitle("Ineffa's Uptime")
+                .setDescription(`Ineffa has been operational for: **${uptime}**.`)
+                .setFooter({ text: "System Status Report." })
+                .setTimestamp();
+
+            await interaction.reply({ embeds: [embed] });
+        },
+};
