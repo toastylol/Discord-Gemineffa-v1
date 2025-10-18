1905ff136b539ac54f4ef8fd886587e49f5d7b46
toastylol <leonardjohn974@gmail.com>
2025-10-18 11:59:02 +0530

    create ping command

---
+++ commands/ping.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription(`Replies with Pong! and shows Ineffa's latency.`),
+
+    async execute(interaction) {
+        const sent = await interaction.reply({ 
+            content: "Calculating latency...", 
+            fetchReply: true 
+        });
+
+        const ping = sent.createdTimestamp - interaction.createdTimestamp;
+        const apiPing = Math.round(interaction.client.ws.ping);
+
+        const embed = new EmbedBuilder()
+            .setColor('Random')
+            .setTitle(`Ineffa's Latency Report`)
+            .addFields(
+                { name: 'Message Latency', value: `${ping}ms`, inline: true },
+                { name: 'API Latency', value: `${apiPing}ms`, inline: true }
+            )
+            .setFooter({ text: "Operational check complete." })
+            .setTimestamp();
+
+            await interaction.editReply({ content: "Pong!", embeds: [embed] });
+    },
+};
